afb92929ab70c2836cea7afe6fbe651ae398769c
Jaiber Diaz <djaiver9@gmail.com>
2023-08-14 07:30:39 -0500

    Tablas rowspam y colspan

---
+++ function.js
@@ -0,0 +1,17 @@
+function ordenarBurbuja() {
+  var input = document.getElementById('burbujaInput').value;
+  // Aquí puedes implementar la lógica de ordenamiento con el algoritmo de Burbuja
+  // y actualizar la entrada con el resultado
+}
+
+function ordenarSeleccion() {
+  var input = document.getElementById('seleccionInput').value;
+  // Aquí puedes implementar la lógica de ordenamiento con el algoritmo de Selección
+  // y actualizar la entrada con el resultado
+}
+
+function ordenarInsercion() {
+  var input = document.getElementById('insercionInput').value;
+  // Aquí puedes implementar la lógica de ordenamiento con el algoritmo de Inserción
+  // y actualizar la entrada con el resultado
+}
