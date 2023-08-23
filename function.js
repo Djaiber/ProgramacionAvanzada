65c059870097433bd32e98c85629953cffb3dcb2
Jaiber Diaz <djaiver9@gmail.com>
2023-08-23 16:06:02 -0500

    function getId

--- function.js
+++ function.js
@@ -1,14 +1,16 @@
-document.getElementById("entradas").addEventListener("submit", function(event) {
-  event.preventDefault(); // Evita el envío del formulario por defecto
+var Algoritmo1 = document.getElementById("Algoritmo1").value;
+var Algoritmo2 = document.getElementById("Algoritmo2").value;
+var Algoritmo3 = document.getElementById("Algoritmo3").value;
+var Algoritmo4 = document.getElementById("Algoritmo4").value;
+var Algoritmo5 = document.getElementById("Algoritmo5").value;
+var Algoritmo6 = document.getElementById("Algoritmo6").value;
+var OtrosAlgoritmos = document.getElementById("OtrosAlgoritmos").value;
 
-  // Captura los valores del formulario
-  var nombre = encodeURIComponent(document.querySelector("input[name='nombre']").value);
-  var email = encodeURIComponent(document.querySelector("input[name='email']").value);
-  
-  // Construye la URL con los parámetros GET
-  var url = "ValoresRecibidos.html=" + nombre + "&email=" + email;
-  
-  // Redirecciona a la otra página
-  window.location.href = url;
-});
+var table = "<table><thead> <th colspan='3'>Algoritmos de Ordenamiento</th></thead><tbody><tr><td rowspan='4'><br><img src='images/sort-algoritms.png' alt='Image' width='220'></td></tr><tr><td>"+Algoritmo1+"</td><td>"+Algoritmo2+"</td></tr><tr><td>"+Algoritmo3+"</td><td>"+Algoritmo4+"</td></tr><tr><td>"+Algoritmo5+"</td><td>"+Algoritmo6+"</td></tr></tbody><tfoot><tr><td colspan='2'><textarea cols='50' id='OtrosAlgoritmos' placeholder='Sí recuerda, agrege otros algoritmos'>"+OtrosAlgoritmos+"</textarea></td><td><button type='submit'><img src='images/icons8-send-button-64.png' />Enviar Datos</button></td></tr></tfoot></table>";
 
+document.getElementById("table_id").innerHTML = table;
+
+function sendTable() {
+  document.getElementById("table_id").innerHTML = table;
+  window.location.href = "Page3.html";
+}
