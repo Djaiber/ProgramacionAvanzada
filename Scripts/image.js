2d5ff8840706fcb62c76b927039be0eaba74b988
Jaiber Diaz <djaiver9@gmail.com>
2023-08-30 16:11:47 -0500

    Funtions

---
+++ Scripts/image.js
@@ -0,0 +1,13 @@
+function previewImage(event) {
+    const preview = document.getElementById('image_preview');
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function() {
+      preview.src = reader.result;
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
